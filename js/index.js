720a87b0755c1f2a4286e538c7256a7b9e45a4d6
Anna Mendrukh <amendrukh@gmail.com>
2023-03-06 18:27:02 +0200

    added new commmit

--- js/index.js
+++ js/index.js
@@ -1,14 +1,12 @@
-const headerEl = document.querySelector(".hamburger");
-const headerBanner = document.querySelector(".banner");
-const headerWrapper = document.querySelector(".nav__wrapper");
+const hamburger = document.querySelector(".hamburger");
+const banner = document.querySelector(".banner");
+const navWrapper = document.querySelector(".nav__wrapper");
+const headerWrapper = document.querySelector(".header__wrapper");
 const header = document.querySelector(".header")
-headerEl.addEventListener("click", (e) => {
-    headerEl.classList.toggle("is-active");
-    headerBanner.classList.toggle("active");
-    headerWrapper.classList.toggle("active");
-    if (headerWrapper.classList.contains("active")) {
-        header.classList.add("header__humburger");
-    } else {
-        header.classList.remove("header__humburger")
-    }
-} )
+hamburger.addEventListener("click", (e) => {
+    hamburger.classList.toggle("is-active");
+    banner.classList.toggle("active");
+    navWrapper.classList.toggle("active");
+    headerWrapper.classList.toggle("header_1");
+    header.classList.toggle("header__humburger");
+})
