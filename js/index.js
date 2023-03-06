9312d63b2c8d96864089397027396bd04a0f314a
Anna Mendrukh <amendrukh@gmail.com>
2023-03-06 18:36:47 +0200

    test

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
-document.querySelector(".hamburger")
-    .addEventListener("click", (e) => {
-        const banner = document.querySelector(".banner");
-        const navWrapper = document.querySelector(".nav__wrapper");
-        const headerWrapper = document.querySelector(".header__wrapper");
-        const header = document.querySelector(".header");
+const hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", () => {
+    const banner = document.querySelector(".banner");
+    const navWrapper = document.querySelector(".nav__wrapper");
+    const headerWrapper = document.querySelector(".header__wrapper");
+    const header = document.querySelector(".header");
 
-        hamburger.classList.toggle("is-active");
-        banner.classList.toggle("active");
-        navWrapper.classList.toggle("active");
-        headerWrapper.classList.toggle("header_1");
-        header.classList.toggle("header__humburger");
-    })
+    hamburger.classList.toggle("is-active");
+    banner.classList.toggle("active");
+    navWrapper.classList.toggle("active");
+    headerWrapper.classList.toggle("header_1");
+    header.classList.toggle("header__humburger");
+})
